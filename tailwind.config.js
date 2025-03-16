a19e87b192713861bdd62a6030132059671c028b
Aimen Chaouli <45677059+aimen-chaouli@users.noreply.github.com>
2025-03-16 01:44:51 +0100

    Updated portfolio files and added resume.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "resume.html", "contact.html"],
   theme: {
     extend: {},
   },
