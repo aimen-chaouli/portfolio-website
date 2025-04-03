f3ddf02358a2e26f188ccef59cd440c2a8152ebc
Aimen Chaouli <45677059+aimen-chaouli@users.noreply.github.com>
2025-04-04 00:28:27 +0100

    Proects Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "resume.html", "contact.html", "projects.html"],
+  content: [
+    "index.html",
+    "resume.html",
+    "contact.html",
+    "projects.html",
+    "project-absence.html",
+    "project-website.html",
+  ],
   theme: {
     extend: {},
   },
